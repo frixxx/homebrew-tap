1de06471b7dcae2b17c83741b6c8bce5f560d8b9
Matthias Vöpel <matthias@voepel.ruhr>
2026-01-08 16:27:50 +0100

    Create azconnect.rb

---
+++ Formula/azconnect.rb
@@ -0,0 +1,11 @@
+class Azconnect < Formula
+  homepage "https://github.com/frixxx/azconnect"
+  url "https://github.com/frixxx/azconnect/archive/refs/tags/v0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 "87b0cebd2bdf1e944db3f4090fd44bc75da2f435a52a2835b6c9084a916e3101"
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_exec_script (libexec/"bin").children
+  end
+end
