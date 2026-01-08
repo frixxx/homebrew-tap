9e92e5a5578f9c37da67b08bd26f7cdf231c2c59
Matthias Vöpel <matthias@voepel.ruhr>
2026-01-08 11:25:29 +0100

    Update backup-user-directory.rb to v0.0.2

--- Formula/backup-user-directory.rb
+++ Formula/backup-user-directory.rb
@@ -1,8 +1,8 @@
 class BackupUserDirectory < Formula
   homepage "https://github.com/frixxx/backup-user-directory"
-  url "https://github.com/frixxx/backup-user-directory/archive/refs/tags/v0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "dbe3d3fae559ae8368fbee82dff3fcf99ac0bada0653621d12613383e9c2c91c"
+  url "https://github.com/frixxx/backup-user-directory/archive/refs/tags/v0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "a46994ef2e87993eea049d706502ca167f614ce4cf6365016b9c7849c497e4a4"
 
   def install
     libexec.install Dir["*"]
