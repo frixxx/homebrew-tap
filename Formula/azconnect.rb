055c8dc47ca915a52f5c306fb90b350d66df5785
Matthias Vöpel <matthias@voepel.ruhr>
2026-01-08 16:31:12 +0100

    added dependency definition

--- Formula/azconnect.rb
+++ Formula/azconnect.rb
@@ -3,6 +3,9 @@ class Azconnect < Formula
   url "https://github.com/frixxx/azconnect/archive/refs/tags/v0.0.1.tar.gz"
   version "0.0.1"
   sha256 "87b0cebd2bdf1e944db3f4090fd44bc75da2f435a52a2835b6c9084a916e3101"
+  depends_on "jq"
+  depends_on "az"
+  depends_on "simple-select"
 
   def install
     libexec.install Dir["*"]
