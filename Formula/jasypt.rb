51bab3b337adad04b04a290e5c3eba66794a312d
Matthias Vöpel <matthias.voepel@eon.com>
2026-06-11 15:03:15 +0200

    added contents for jasypt.rb

--- Formula/jasypt.rb
+++ Formula/jasypt.rb
@@ -1,8 +1,8 @@
 class Jasypt < Formula
-  homepage "https://github.com/<project>"
-  url "https://github.com/<project>.tgz"
+  homepage "https://github.com/frixxx/jasypt"
+  url "https://github.com/frixxx/jasypt/archive/refs/tags/v0.0.1.tar.gz"
   version "v0.0.1"
-  sha256 "<shasum>"
+  sha256 "a6ba0c5b091fca6c4e985a3b0c95b949204188a8629eb1924de558cdc92677da"
 
   def install
     libexec.install Dir["*"]
